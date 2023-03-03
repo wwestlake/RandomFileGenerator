3155a2c9ff7a78a7ae306433d948cdf12ad7c738
Bill Westlake <wwestlake@lagdaemon.com>
2023-03-03 15:56:06 -0500

    first commit

---
+++ TExtFileGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RandomFileGenerator;
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+
+Console.WriteLine("Generating file");
+
+
+CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+ParsingTarget p = new ParsingTarget();
+    try
+    {
+        parser.ExtractArgumentAttributes(p);
+        parser.ParseCommandLine(args);
+
+        Console.WriteLine($"Generating file {p.Filename} of size {p.Size} {p.Unit} -- {p.FileType}");
+
+    }
+    catch (Exception ex)
+    {
+
+        parser.ShowUsageHeader = "Here is how you use the app: ";
+        parser.ShowUsageFooter = "Have fun!";
+        parser.ShowUsage();
+    }
+
